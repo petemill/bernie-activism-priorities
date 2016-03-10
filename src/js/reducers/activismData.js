25e25ebe5fe2df6c8df195f33ecd8f38e7ec6521
petemill <miller.pete+commit@gmail.com>
2016-03-09 20:33:30 -0800

    load initial data from .json file

--- src/js/reducers/activismData.js
+++ src/js/reducers/activismData.js
@@ -1,22 +1,14 @@
+const localData = require("../../../data/actions.json");
+
 
 const initialState = {
-  actionsByState: {
-    AK: {
-      Summary: 'hi'
-    }
-  },
-  priorityActions: [
-    {
-      Summary: 'do this'
-    }
-  ],
+  ...localData,
   chosenState: null
 };
 
 export default function (state = initialState, action) {
 
   switch (action.type) {
-
     case 'SELECT_STATE_BYID':
       return {
         ...state,
