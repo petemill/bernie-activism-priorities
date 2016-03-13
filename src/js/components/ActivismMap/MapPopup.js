e032bb0a7fc1ff2448ee652698d07e386d9cb05b
petemill <miller.pete+commit@gmail.com>
2016-03-12 20:05:18 -0800

    add action summaries to map popup

--- src/js/components/ActivismMap/MapPopup.js
+++ src/js/components/ActivismMap/MapPopup.js
@@ -12,7 +12,7 @@ function GetMapPopupHtml(geo, stateData) {
     htmlDelegatesWon = `<div class="delegate-info">Delegates won: <span class="delegate-count-bernie">${data.delegatesWon || 0}</span> / <span class="delegate-count-total">${data.delegateTotal}</span> (<span class="delegate-percentagewon">${delegatePercentageWon}%</span>)</div>`
   }
   //actions html
-  const htmlActions = data.ActionData ? getMapPopupActionData(data.ActionData) : null;
+  const htmlActions = data.ActionData ? getMapPopupActionData(data.ActionData) : undefined;
   //primary date html
   let htmlPrimaryDateHtml = null;
   if (data.PrimaryDate) {
@@ -23,9 +23,19 @@ function GetMapPopupHtml(geo, stateData) {
   //compile all html
   return `<div class="activism-map-popup">
             <h3 class="state-name">${data.Name}</h3>
-            ${htmlPrimaryDateHtml}
-            ${htmlDelegatesWon}
+            ${htmlPrimaryDateHtml || ''}
+            ${htmlDelegatesWon || ''}
+            ${htmlActions || ''}
+            <p class="message-selectstate">Click state to see details</p>
           </div>`;
+}
+
+function getMapPopupActionData(actionData) {
+
+  if (actionData.Actions && actionData.Actions.length) {
+    return actionData.Actions.map(action => `<p class="action-summary">${action.Summary}</p>`).join();
+  }
+  return undefined;
 }
 
 export default GetMapPopupHtml;
