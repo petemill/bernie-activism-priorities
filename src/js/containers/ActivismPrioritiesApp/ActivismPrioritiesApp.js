053c62a2e8a17cd5f692adafea6dd8af5945763a
Kat Windley <katherine.windley@gmail.com>
2016-03-13 00:00:05 +0000

    selected state data appears above map

--- src/js/containers/ActivismPrioritiesApp/ActivismPrioritiesApp.js
+++ src/js/containers/ActivismPrioritiesApp/ActivismPrioritiesApp.js
@@ -37,8 +37,8 @@ class ActivismPrioritiesApp extends Component {
       <div className="app-activsm-priorities">
         <h1>Activism for Bernie Sanders</h1>
         {priorityActionsComponents}
-        <ActivismMap stateData={stateData} chosenState={chosenState} chooseStateById={actions.selectState} />
-        {chosenStateComponents}
+	      {chosenStateComponents}
+        <ActivismMap stateData={stateData} chosenState={chosenState} chooseStateById={actions.selectState} />      
       </div>
     );
   }
